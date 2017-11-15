273f4070349bd62bf50262c7999de7ed3713464c
Raja Vyshnavi Sriramoju <rsriramoju@ucdavis.edu>
2017-11-15 15:43:50 -0800

    Test Submission

--- src/main/test/core/test/ExampleTest.java
+++ src/main/test/core/test/ExampleTest.java
@@ -30,4 +30,7 @@ public class ExampleTest {
         this.admin.createClass("Test", 2016, "Instructor", 15);
         assertFalse(this.admin.classExists("Test", 2016));
     }
+    
+    //label the test funcs as @Test
+    //Make 3 classes for every class you will be testing and testmethod for every method
 }
